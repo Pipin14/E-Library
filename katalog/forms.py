2d24e7696ad8b63827b8503f121bf490db39941c
Pipin14 <kang.musjr.20@gmail.com>
2024-11-15 12:45:02 +0700

    Create Upload Books on Catalog Views

--- katalog/forms.py
+++ katalog/forms.py
@@ -1,11 +1,38 @@
 
+# from django import forms
+# from .models import Book
+
+# # class BookUploadForm(forms.ModelForm):
+# #     class Meta:
+# #         model = Book
+# #         fields = ['cover_image', 'title', 'description', 'author', 'genre', 'publication_year', 'page_count']
+# #         widgets = {
+# #             'cover_image': forms.ClearableFileInput(attrs={'accept': 'image/*'}),
+# #         }
+
+
+# class BookUploadForm(forms.ModelForm):
+#     pdf_file = forms.FileField(required=True, label='File PDF')
+
+#     class Meta:
+#         model = Book
+#         fields = ['pdf_file', 'title', 'description', 'author', 'publication_year', 'genre']
+import logging
 from django import forms
 from .models import Book
+
+logger = logging.getLogger(__name__)
 
 class BookUploadForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['cover_image', 'title', 'description', 'author', 'genre', 'publication_year', 'page_count']
-        widgets = {
-            'cover_image': forms.ClearableFileInput(attrs={'accept': 'image/*'}),  # Menambahkan file upload untuk gambar
-        }
+        fields = ['pdf_file', 'title', 'description', 'author', 'publication_year', 'genre', 'page_count']
+
+    def save(self, commit=True):
+        logger.debug("Form disubmit dan data akan disimpan")
+        instance = super().save(commit=False)
+        
+        if commit:
+            instance.save()
+            logger.debug(f"Buku berhasil disimpan dengan ID: {instance.id}")
+        return instance
--- katalog/models.py
+++ katalog/models.py
@@ -1,15 +1,28 @@
-
+import logging
 from django.db import models
+
+logger = logging.getLogger(__name__)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    author = models.CharField(max_length=200)  # Penulis
-    genre = models.CharField(max_length=100)  # Genre
-    publication_year = models.IntegerField()  # Tahun Terbit
-    page_count = models.IntegerField()  # Jumlah Halaman
-    cover_image = models.ImageField(upload_to='books/covers/')  # Thumbnail
-    is_favorite = models.BooleanField(default=False)  # Untuk tanda buku favorit
+    author = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    page_count = models.IntegerField()
+    cover_image = models.ImageField(upload_to='books/covers/')
+
+    pdf_file = models.FileField(upload_to='books/pdf', blank=True, null=True)
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        logger.debug(f"Saving book: {self.title}")
+        if self.pdf_file:
+            logger.debug(f"PDF file: {self.pdf_file.name}")
+        if self.cover_image:
+            logger.debug(f"Cover image: {self.cover_image.name}")
+        super().save(*args, **kwargs)
